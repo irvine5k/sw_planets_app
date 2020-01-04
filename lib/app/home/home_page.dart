efe6757cfd9b0b3e2ebcd2b5b3957386d40f08d2
danielmessi13 <danielmessi13@hotmail.com>
2020-01-04 15:47:13 -0300

    [WIP] Tela inicial

---
+++ lib/app/home/home_page.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Image.asset(
+            "assets/moon.jpg",
+            fit: BoxFit.fill,
+            width: MediaQuery.of(context).size.width,
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 30, top: 50),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "Planets",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 17,
+                  ),
+                ),
+                SizedBox(height: 40),
+                Text(
+                  "FRASE DE EFEITO",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  children: <Widget>[
+                    Stack(
+                      children: <Widget>[
+                        Container(
+                          height: 50,
+                          width: 50,
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+                          ),
+                        ),
+                        Icon(
+                          Icons.add_circle,
+                          color: Colors.white,
+                          size: 50,
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 12.0),
+                      child: Text(
+                        "Ver todos",
+                        style: TextStyle(
+                          color: Colors.grey[200],
+                          fontSize: 14,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Text(
+                  "Planetas Destaque",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  "dasdas",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+                Container(
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: <Widget>[
+                      Container(
+                        color: Colors.red,
+                        width: 50,
+                        height: 90,
+                      ),
+                      Container(
+                        color: Colors.red,
+                        width: 50,
+                        height: 90,
+                      ),
+                      Container(
+                        color: Colors.red,
+                        width: 50,
+                        height: 90,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
